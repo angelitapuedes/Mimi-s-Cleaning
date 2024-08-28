c6bbae97f9165b4ae5a352108953fa87b9fa191d
Angela Rodriguez <angiemarirodriguez@gmail.com>
2024-08-28 16:22:03 -0400

    Added a form

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Secret Key"
+
+class NamerForm(FlaskForm):
+    name = StringField("Whats your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 
@@ -22,3 +30,13 @@ def page_not_found(e):
 @app.errorhandler(505)
 def page_not_found(e):
     return render_template("505.html"), 505
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", form=form)
